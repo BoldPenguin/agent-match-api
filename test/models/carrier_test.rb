3e9df7c77896927a6dc70273d27c1ff06631211e
Mike Larkin <mikelarkin@users.noreply.github.com>
2020-01-28 14:53:26 -0500

    Upgraded to rails 6 / latest gems / added rubocop (#2)

--- test/models/carrier_test.rb
+++ test/models/carrier_test.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class CarrierTest < ActiveSupport::TestCase
   test 'should validate name is present' do
-    carrier = Carrier.find_or_create_by(name: nil)
+    carrier = Carrier.new(name: nil)
     assert_not carrier.valid?
   end
   test 'Big City should have an industry' do
-    carrier = Carrier.find_by_name('Big City Mutual Insurance')
+    carrier = Carrier.find_by(name: 'Big City Mutual Insurance')
     assert_equal 1, carrier.industries.size
   end
 end
--- test/models/license_test.rb
+++ test/models/license_test.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class LicenseTest < ActiveSupport::TestCase
   test 'should validate state is present' do
-    license = License.find_or_create_by(state: nil)
+    license = License.new(state: nil)
     assert_not license.valid?
   end
 end
